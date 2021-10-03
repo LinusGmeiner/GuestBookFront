52b99ebd435e4a05baad0f701d643ea313d9ef3b
Linus <lgm1768@students.fhv.at>
2021-10-03 17:12:32 +0200

    changes

--- GuestBookFront/Model/GuestBookEntry.cs
+++ GuestBookFront/Model/GuestBookEntry.cs
@@ -9,9 +9,9 @@ namespace GuestBookFront.Model
     {
         public long Id { get; set; }
         public string Title { get; set; }
-        public string Text { get; set; }
-        public string Visitor { get; set; }
-        public DateTime Created { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public DateTime Time { get; set; }
     }
 
     public class GuestBookList
